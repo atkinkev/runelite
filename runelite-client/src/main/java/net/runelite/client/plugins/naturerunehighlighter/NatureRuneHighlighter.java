74fc8e6c516117633d5a90d2d8cb0f52c90add5e
Kevin Atkins <atkinkev@oregonstate.edu>
2019-09-14 18:53:12 -0500

    Experimenting with inventory tracking

--- runelite-client/src/main/java/net/runelite/client/plugins/naturerunehighlighter/NatureRuneHighlighter.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/naturerunehighlighter/NatureRuneHighlighter.java
@@ -1,13 +1,10 @@
-package net.runelite.client.plugins.myfirstplugin;
+package net.runelite.client.plugins.naturerunehighlighter;
 
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
-import net.runelite.api.Item;
 import net.runelite.api.Skill;
-import net.runelite.api.World;
 import net.runelite.api.events.ExperienceChanged;
-import net.runelite.api.events.ItemContainerChanged;
+import net.runelite.api.events.GameTick;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
@@ -15,15 +12,15 @@ import net.runelite.client.ui.overlay.OverlayManager;
 
 import javax.inject.Inject;
 
+import static net.runelite.api.ScriptID.XPDROP_DISABLED;
+
 @Slf4j
 @PluginDescriptor(
         name = "First Plugin",
         description = "Congratulates you for all XP"
 )
 
-public class MyFirstPlugin extends Plugin {
-    @Getter
-    private Item[] equippedItems;
+public class NatureRuneHighlighter extends Plugin {
     @Inject
     private Client client;
     @Inject
@@ -32,30 +29,15 @@ public class MyFirstPlugin extends Plugin {
     @Inject
     private MyOverlay myOverlay;
 
-    @Inject
-    private World world;
-
     @Override
     public void startUp() {
         overlayManager.add(myOverlay);
-        client.changeWorld(world);
     }
 
     @Override
     public void shutDown() {
         overlayManager.remove(myOverlay);
     }
-
-    @Subscribe
-    public void onItemContainerChanged(final ItemContainerChanged event){
-        equippedItems = event.getItemContainer().getItems();
-        for (int i = 0; i < equippedItems.length; i++) {
-            if (equippedItems[i].getId() != -1) {
-                System.out.println("You are carrying: " + equippedItems[i].toString());
-            }
-        }
-    }
-
 
     @Subscribe
     public void onExperienceChanged(ExperienceChanged event){
@@ -66,6 +48,13 @@ public class MyFirstPlugin extends Plugin {
         if (level != 0){
             System.out.println("Congrats on gaining xp. I'm really proud! " + skill.getName() + " is now at: " + xp + " XP");
         }
+    }
+
+    @Subscribe
+    public void onGameTick(GameTick tick)
+    {
+       // currentTickPrayer = getInventoryArray();
+
     }
 
     int getFiremakingLevel(){
